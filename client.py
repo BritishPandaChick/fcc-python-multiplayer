44766d277d39e391a61bcb9f9506777c6b3afaf3
BritishPandaChick <sbartley1080@gmail.com>
2020-05-19 16:57:26 -0400

    Add update function to client file. Add code for players 1 and 2.Add read_pos and make_pos functions to server file

--- client.py
+++ client.py
@@ -36,6 +36,9 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(str):
         self.rect = (self.x, self.y, self.width, self.height)
 
 def read_pos(str):
@@ -45,9 +48,10 @@ def read_pos(str):
 def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
-def redrawWindow(win, player):
+def redrawWindow(win, player, player2):
     win.fill((255, 255, 255))
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
@@ -55,17 +59,23 @@ def main():
     run = True
     n = Network()
     startPos = read_pos(n.getPos())
-    p = Player(startPos[0], 50,50,100,100,(0, 255, 0))
+    p = Player(startPos[0], startPos[1], 100,100,(0, 255, 0))
+    p2 = Player(0,0, 100,100,(0, 255, 0))
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False 
                 pygame.quit() 
 
         p.move()
-        redrawWindow(win, p)
+        redrawWindow(win, p, p2)
 
 main()
